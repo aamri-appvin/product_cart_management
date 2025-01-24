e92092afe51c71b327b8bdd9f3c84dfe5c9bd702
aamri appvin <amri@appvintech.com>
2025-01-24 18:28:41 +0530

    updating env

--- database.py
+++ database.py
@@ -1,8 +1,15 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-DATABASE_URL = "postgresql+asyncpg://myuser:NewPassword123!@localhost/new_db"
+load_dotenv()
+user=os.getenv("USER")
+password=os.getenv("PASSWORD")
+database=os.getenv("DATABASE")
+
+DATABASE_URL = "postgresql+asyncpg://{user}:{password}@localhost/{database}"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
