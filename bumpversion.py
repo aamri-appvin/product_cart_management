781dec414fdd5697cee131c8c2b8a169d37554a8
aamri appvin <amri@appvintech.com>
2025-02-14 12:16:36 +0530

    using cli to update versions automatically

---
+++ bumpversion.py
@@ -0,0 +1,64 @@
+import version
+import json 
+import os
+from datetime import datetime
+import subprocess
+import argparse
+
+VERSION_FILE="version.json"
+
+def load_file():
+    if os.path.exists(VERSION_FILE):
+        with open(VERSION_FILE,"r") as file:
+            return json.load(file)
+    return {}
+
+def save_version(version_data):
+    with open(VERSION_FILE,"w") as file:
+        json.dump(version_data,file,indent=4)
+    
+
+
+def update_version(component,version):
+
+    if not os.path.exists(VERSION_FILE):
+        raise ValueError("Version File Does Not Exist")
+    
+    version_data=load_file()
+
+    if component not in version_data:
+        raise ValueError("Component not in version file")
+    
+    current_version=version_data[component]
+
+    print(f"v{current_version} is your current version")
+
+    if not isinstance(current_version,str):
+        current_version=str(current_version)
+
+    if not current_version.count(".")==2:
+        raise ValueError("Invalid version!")
+    
+    version_data[component]=version
+    version_data["release_date"]=datetime.today().strftime("%Y-%m-%d")
+    version_data["description"]=f"Version is updated for {component} from v{current_version} to v{version}"
+    commit_msg=f"Version is updated for {component} from v{current_version} to v{version}"
+    git_tag=f"{component}-v{version}"
+    save_version(version_data)
+    try:
+        subprocess.run(["git","add",VERSION_FILE],check=True)
+        subprocess.run(["git","commit","-m",commit_msg],check=True)
+        subprocess.run(["git","tag",git_tag],check=True)
+        subprocess.run(["git","push","origin","master"],check=True)
+        subprocess.run(["git","push","origin",git_tag],check=True)
+    except subprocess.CalledProcessError as error:
+        raise RuntimeError(f"Git command failed with error {error}")
+
+    print(f"v{version} is the updated version")
+
+if __name__=="__main__":
+    parser=argparse.ArgumentParser(description="To update the version of a specific component")
+    parser.add_argument("component", help="Component to update")
+    parser.add_argument("version", help="New version number")
+    args=parser.parse_args()
+    update_version(args.component,args.version)
